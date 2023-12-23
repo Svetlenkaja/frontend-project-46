45296e6dab18f8005f8e0cc30dfcd85cfba458e0
svetlenkaja <svetlenkajas@gmail.com>
2023-12-23 12:56:50 -0800

    step2

--- gendiff.js
+++ gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 
 program
 .version('1.0.0')
-.description('Compares two configuration files and shows a difference.');
+.description('Compares two configuration files and shows a difference.')
+.argument('<filepath1>')
+.argument('<filepath2>')
+.option('-f, --format <type>', 'output format');
 
 program.parse();
